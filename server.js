6aff48a3d483a8a913f53d6c436df13b0181b4e9
Brendan Halliday <nog3@nog3.net>
2014-01-23 22:32:31 -0500

    Fixing function evaluation order so HTTP server actually runs.

--- server.js
+++ server.js
@@ -13,13 +13,26 @@ var bot = new irc.Client('chat.freenode.net', 'HSBNEBot' || 'borkbott', {
     showerrors: true
 });
 
+var port = process.env.OPENSHIFT_NODEJS_IP || 8080;
+//    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
+http.createServer(function(request,response){  
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.write("Hello I are IRCbot!");
+        response.end();
+}).listen(port);  
+
+console.log("Server Running on " + port); 
+
+bot.addListener('error', function(message) {bot.say('#hsbne', message);});
+
 bot.addListener('message', function(from, to, message) {
     if(  message.indexOf('http') > -1 ) {
             var regex = new RegExp(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/); 
                         if ( regex.test(message)) {
                         var httpurl = message.match(regex);
                         // Request URL's title.
-                                linksnip(httpurl[0].toString(), function(results){
+                        linksnip(httpurl[0].toString(), function(results){
                                 bot.say(to, "Title: " + JSON.stringify(results.title, null, 2));
                             });
                             
@@ -30,15 +43,4 @@ bot.addListener('message', function(from, to, message) {
     }
 });
         
-bot.addListener('error', function(message) {bot.say('#hsbne', message);});
 
-var port = process.env.OPENSHIFT_NODEJS_IP || 8080,
-    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-    
-http.createServer(function(request,response){  
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Hello I are IRCbot!");
-        response.end();
-}).listen(port, ip);  
-
-console.log("Server Running on " + port); 
