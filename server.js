8614c51107809d1148df039296ed439e1499b10a
Brendan Halliday <nog3@nog3.net>
2014-01-23 19:43:08 -0500

    Changed to node-irc module with pingtimeout. Moved url checking to cheerio + request.

--- server.js
+++ server.js
@@ -1,31 +1,28 @@
 //HSBNE IRC Bot
-var http = require("http")
-var irc = require('irc');
+var http = require("http"),
+    irc = require('irc'),
+    linksnip = require("linksnip"),
+    url = require('url'),
+    request = require('request');
 var bot = new irc.Client('chat.freenode.net', 'HSBNEBot' || 'borkbott', {
     botName: 'botbott',
     userName: 'hsbnebot',
-    channels: ['#hsbne'],
+    channels: ['#hsbnebot'],
     port: 8001,
     debug: true,
     showerrors: true
 });
 
-var YQL = require("yql");
- 
 bot.addListener('message', function(from, to, message) {
     if(  message.indexOf('http') > -1 ) {
             var regex = new RegExp(/(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?/); 
                         if ( regex.test(message)) {
                         var httpurl = message.match(regex);
-                        // Request URL's title using YQL
-                        
-                        new YQL.exec('select * from data.html.cssselect where url="' + httpurl[0].toString() + '" and css="title"', function(response) {
-                            if (response.query.results.results && typeof response.query.results.results.title != 'undefined') {
-                            var title = response.query.results.results.title;
-                            // Say the resulting title.
-                            bot.say(to, "Title: " + title);
-                            }
-                                                    });
+                        // Request URL's title.
+                                linksnip(httpurl[0].toString(), function(results){
+                                bot.say(to, "Title: " + JSON.stringify(results.title, null, 2));
+                            });
+                            
                         }
     }
     if(  message.indexOf('we should') > -1 ) {
@@ -35,11 +32,12 @@ bot.addListener('message', function(from, to, message) {
         
 bot.addListener('error', function(message) {bot.say('#hsbne', message);});
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_IP || "8080";
+//    ip = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 http.createServer(function(request,response){  
         response.writeHead(200, { "Content-Type": "text/plain" });
         response.write("Hello I are IRCbot!");
         response.end();
-}).listen(port, ip);  
+//}).listen(port, ip);  
+}).listen(port);  
 console.log("Server Running on " + port); 
